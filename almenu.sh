cd19ac0239fb48db2a4ceb4c257e1c2d7effd71d
yusuf-ardiansyah <yusuf-ardiansyah@users.noreply.github.com>
2017-04-24 18:24:03 +0700

    Update almenu.sh

--- almenu.sh
+++ almenu.sh
@@ -11,26 +11,26 @@ echo "Instalasi file pewarna text sysrem"
 # text sistem warna
 cd
 rm -rf .bashrc
-wget https://raw.githubusercontent.com/deeniedoank/autoscript2/master/text%20warna/.bashrc
+wget https://raw.githubusercontent.com/yusuf-ardiansyah/premium-edition/master/text%20warna/.bashrc
 echo "Instalasi all menu PREMIUM"
 
 # menu
 cd
-wget https://raw.githubusercontent.com/deeniedoank/autoscript2/master/menu/menu
+wget https://raw.githubusercontent.com/yusuf-ardiansyah/premium-edition/master/menu/menu
 mv ./menu /usr/local/bin/menu
 chmod +x /usr/local/bin/menu
 
 echo "Instalasi motd"
 #motd
 cd
-wget "https://raw.githubusercontent.com/deeniedoank/autoscript2/master/menu/motd"
+wget "https://raw.githubusercontent.com/yusuf-ardiansyah/premium-edition/master/menu/motd"
 mv ./motd /etc/motd
 
 echo "Instalasi speedtest"
 #speedtest
 cd
 apt-get install python
-wget -O speedtest.py "https://raw.githubusercontent.com/deeniedoank/autoscript2/master/menu/speedtest.py"
+wget -O speedtest.py "https://raw.githubusercontent.com/yusuf-ardiansyah/premium-edition/master/menu/speedtest.py"
 chmod +x speedtest.py
 
 echo "Selesai...!!!";
